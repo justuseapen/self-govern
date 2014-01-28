4b7d2145a69a5c8cb3bdf33eccab7621b918da11
Justus Danny Eapen Jr <justuseapen@gmail.com>
2014-01-27 23:54:54 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source "http://rubygems.org"
+
+gem "rails", "4.0.2"
+gem "pg"
+gem "sass-rails", "~> 4.0.0"
+gem "uglifier", ">= 1.3.0"
+gem "coffee-rails", "~> 4.0.0"
+gem "jquery-rails"
+gem "jbuilder", "~> 1.2"
+group :doc do
+  gem "sdoc", require: false
+end
+
+group :development, :test do
+  gem "pry-rails"
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem "meta_request"
+  gem "rspec-rails"
+  gem "factory_girl_rails"
+  gem "shoulda-matchers"
+  gem "valid_attribute"
+  gem "capybara"
+  gem "quiet_assets"
+  gem "launchy"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SelfGovern::Application.config.secret_key_base = 'f96a5942d21b6d881d19d57c91243c21ca07e0a0a01e41fb8574798a8836549840961ab2a17a2f4c3eb40bf84d6fa8be7eb2b49005426b6cc690a72bb7f732e4'
