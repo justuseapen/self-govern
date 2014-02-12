4556bd6c1cafe52c1d4e8276daf4904ab984b043
Daniel Carter <geddoe@gmail.com>
2014-02-12 13:51:31 -0500

    New accounts must confirm email

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SelfGovern::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :test
 end
